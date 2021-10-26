785b6f0ca75c697d4047ec56931e5febdc85344b
Raevin Dunson <raevin.dunson98@gmail.com>
2021-10-25 20:41:48 -0400

    header and footer updates

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,15 +3,14 @@ import coverImage from '../../assets/cover/cover-image.jpg';
 function About() {
   return (
     <section className="my-5">
-      <h1 id="about">Contact Me</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <h2 id="about">Contact Me</h2>
       <div className="my-2">
-          <h2>
+          <h3>
               GitHub: <a href="https://github.com/raevindunson">raevindunson</a>
-          </h2>
-          <h2>
+          </h3>
+          <h3>
               Email: <a href="mailto:raevin.dunson98@gmail.com">raevin.dunson98@gmail.com</a>
-          </h2>
+          </h3>
       </div>
     </section>
   );
