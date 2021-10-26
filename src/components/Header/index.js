af41b3b972de238db46199ac4df06c65a77bd1ac
Raevin Dunson <raevin.dunson98@gmail.com>
2021-10-25 20:45:59 -0400

    header updates

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import coverImage from '../../assets/cover/cover-image.jpg';
+function Header() {
+  return (
+    <section>
+      <h1 id="header">The React Portfolio of Raevin Dunson</h1>
+      <div>
+        <h2>
+            Welcome to my React Portfolio. Click the Links to see my work.
+        </h2>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
